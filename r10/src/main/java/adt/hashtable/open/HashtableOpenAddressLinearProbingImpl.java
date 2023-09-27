724974b9edb0d25f4f3582105d2c7825b4b0e1e6
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-09-27 16:28:25 -0300

    implementar open hashtable linear

--- r10/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
+++ r10/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -2,6 +2,7 @@ package adt.hashtable.open;
 
 import adt.hashtable.hashfunction.HashFunctionClosedAddressMethod;
 import adt.hashtable.hashfunction.HashFunctionLinearProbing;
+import adt.hashtable.hashfunction.HashFunctionOpenAddress;
 
 public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 		AbstractHashtableOpenAddress<T> {
@@ -13,28 +14,72 @@ public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
 		this.initiateInternalTable(size);
 	}
 
+	private int hash(T element, int probe) {
+		return ((HashFunctionOpenAddress<T>) hashFunction).hash(element, probe);
+	}
+
 	@Override
 	public void insert(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (element != null) {
+			int i = 0;
+			boolean found = false;
+			while (!found && i < this.capacity()) {
+				int hash = hash(element, i);
+
+				if (table[hash] == null || table[hash].equals(this.deletedElement)) {
+					found = true;
+					table[hash] = element;
+					++elements;
+				} else {
+					COLLISIONS++;
+					++i;
+				}
+			}
+			if (!found)
+				throw new HashtableOverflowException();
+		}
 	}
 
 	@Override
 	public void remove(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int i = 0;
+		boolean found = false;
+		while (!found && i < this.capacity()) {
+			int hash = hash(element, i);
+
+			if (table[hash] != null && table[hash].equals(element)) {
+				found = true;
+				table[hash] = deletedElement;
+				--elements;
+			} else {
+				++i;
+			}
+		}
 	}
 
 	@Override
 	public T search(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int index = indexOf(element);
+		T result = null;
+		if (index != -1)
+			result = (T) table[index];
+		return result;
 	}
 
 	@Override
 	public int indexOf(T element) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		int result = -1;
+		int i = 0;
+		while (result == -1 && i < this.capacity()) {
+			int hash = hash(element, i);
+
+			if (table[hash] != null && table[hash].equals(element)) {
+				result = hash;
+			} else {
+				++i;
+			}
+		}
+		return result;
 	}
 
 }
