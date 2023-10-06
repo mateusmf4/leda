13dbe97514d8f5d5a260d90d0cb3068cf1e4a8b9
Mateus <130506942+mateusmf4@users.noreply.github.com>
2023-10-06 10:50:06 -0300

    implementar insert e min max

---
+++ r11/src/main/java/adt/bst/BSTImpl.java
@@ -0,0 +1,143 @@
+package adt.bst;
+
+import adt.bt.BTNode;
+
+public class BSTImpl<T extends Comparable<T>> implements BST<T> {
+
+	protected BSTNode<T> root;
+
+	public BSTImpl() {
+		root = new BSTNode<T>();
+	}
+
+	public BSTNode<T> getRoot() {
+		return this.root;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return root.isEmpty();
+	}
+
+	@Override
+	public int height() {
+		return height(root);
+	}
+
+	private int height(BSTNode<T> node) {
+		int result = -1;
+		if (!node.isEmpty()) {
+			result = Math.max(result, height((BSTNode<T>) node.getLeft()));
+			result = Math.max(result, height((BSTNode<T>) node.getRight()));
+			result++;
+		}
+		return result;
+	}
+
+	@Override
+	public BSTNode<T> search(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void insert(T element) {
+		insert(root, element);
+	}
+
+	private void insert(BSTNode<T> node, T element) {
+		if (node.isEmpty()) {
+			node.setData(element);
+			node.setLeft(new BSTNode<>());
+			node.setRight(new BSTNode<>());
+			node.getLeft().setParent(node);
+			node.getRight().setParent(node);
+		} else if (node.getData().compareTo(element) > 0) {
+			insert((BSTNode<T>) node.getLeft(), element);
+		} else {
+			insert((BSTNode<T>) node.getRight(), element);
+		}
+	}
+
+	@Override
+	public BSTNode<T> maximum() {
+		return maximum(root);
+	}
+
+	private BSTNode<T> maximum(BSTNode<T> node) {
+		if (node.isEmpty() || node.getRight().isEmpty()) {
+			return node;
+		} else {
+			return maximum((BSTNode<T>) node.getRight());
+		}
+	}
+
+	@Override
+	public BSTNode<T> minimum() {
+		return minimum(root);
+	}
+
+	private BSTNode<T> minimum(BSTNode<T> node) {
+		if (node.isEmpty() || node.getLeft().isEmpty()) {
+			return node;
+		} else {
+			return minimum((BSTNode<T>) node.getLeft());
+		}
+	}
+
+	@Override
+	public BSTNode<T> sucessor(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public BSTNode<T> predecessor(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public void remove(T element) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] preOrder() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] order() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	@Override
+	public T[] postOrder() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not implemented yet!");
+	}
+
+	/**
+	 * This method is already implemented using recursion. You must understand
+	 * how it work and use similar idea with the other methods.
+	 */
+	@Override
+	public int size() {
+		return size(root);
+	}
+
+	private int size(BSTNode<T> node) {
+		int result = 0;
+		// base case means doing nothing (return 0)
+		if (!node.isEmpty()) { // indusctive case
+			result = 1 + size((BSTNode<T>) node.getLeft())
+					+ size((BSTNode<T>) node.getRight());
+		}
+		return result;
+	}
+
+}
